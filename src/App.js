f5f57a905161adcb24fa804139a4bba599691dce
Dell Pc <mudasir9757@gmail.com>
2023-01-21 11:52:26 +0500

    Add Navigation Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
 import Books from './components/BookPage';
-
+import Category from './components/Category';
 
 export default function App() {
   return (
-    <>
       <Router>
+        <Navigation />
         <Routes>
           <Route path="/" element={<Books />} />
+          <Route path="/Category" element={<Category />} />
         </Routes>
       </Router>
-    </>
   );
 }
