810a976d01e7a6ad161ea5db2666b6502f9f7b2c
Dell Pc <mudasir9757@gmail.com>
2023-01-26 12:43:31 +0500

    Add cominded Reducer configStore

---
+++ src/redux/configureStore.js
@@ -0,0 +1,13 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import reducerForCategories from './categories/categories';
+import reducerForBooks from './books/books';
+
+const reducerCombined = combineReducers({
+  books: reducerForBooks,
+  categories: reducerForCategories,
+});
+
+const configStore = configureStore({
+    reducerCombined,
+});
+export default configStore;
