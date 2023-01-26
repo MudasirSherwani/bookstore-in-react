8182c76907507ff23f25ee7ad1c94a73a128666d
Dell Pc <mudasir9757@gmail.com>
2023-01-26 15:26:47 +0500

    Add book array in book reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,14 +1,19 @@
 const ADD_BOOK = 'redux/books/BOOK_ADDED';
 const REMOVE_BOOK = 'redux/books/BOOK_REMOVED';
 const initialState = {
-  books: [],
+  bookCounts: 3,
+  books: [
+    { key: '1', title: 'The Innovator’s Dilemma', author: 'Clayton Christens' },
+    { key: '2', title: 'Hundred Years of Solitude', author: 'Gabriel Garcia Marquez' },
+    { key: '3', title: 'The Soul of a New Machine', author: 'racy Kidder' },
+  ],
 };
 export const addNewBook = (addNewValue) => (
   { type: ADD_BOOK, addNewValue }
 );
 
-export const removeBook = (id) => (
-  { type: REMOVE_BOOK, id }
+export const removeBook = (key) => (
+  { type: REMOVE_BOOK, key }
 );
 
 const reducerForBooks = (state = initialState, action) => {
@@ -16,12 +21,13 @@ const reducerForBooks = (state = initialState, action) => {
     case ADD_BOOK:
       return {
         ...state,
-        books: [...state, action.payload],
+        bookCounts: state.bookCounts + 1,
+        books: [...state.books, action.addNewValue],
       };
     case REMOVE_BOOK:
       return {
         ...state,
-        books: [...state.filter((book) => book.id !== action.payload.id)],
+        books: [...state.books.filter((book) => book.key !== action.key)],
       };
     default:
       return state;
