6941ea52d266ca87f0e02bc219246676f834480c
Dell Pc <mudasir9757@gmail.com>
2023-01-27 12:36:08 +0500

    Add remove book in component

--- src/components/BookData.js
+++ src/components/BookData.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
-const BookData = ({ Title, Author, Category }) => (
+const BookData = (props) => {
+  const { key, Title, Author } = props;
+  const dispatchBooks = useDispatch();
+  const removeEvent = () => {
+    dispatchBooks(removeBook(key));
+  };
+
+  return (
   <div className="book-item">
     <li>
-      <h3>{Category}</h3>
       <h2>{Title}</h2>
       <h4>{Author}</h4>
-      <button type="button">Remove</button>
+      <button type="button"  onClick={removeEvent}>Remove</button>
     </li>
   </div>
-);
+  );
+};
 
+BookData.propTypes = {
+  Title: PropTypes.string.isRequired,
+  Author: PropTypes.string.isRequired,
+  key: PropTypes.string.isRequired,
+};
 export default BookData;
