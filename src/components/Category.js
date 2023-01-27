5f26d16e209e6e3c61ed3b789b8ba983aea7fcc0
Dell Pc <mudasir9757@gmail.com>
2023-01-27 23:07:11 +0500

    Error Fixed Suggested by code reviewer 2

--- src/components/Category.js
+++ src/components/Category.js
@@ -6,14 +6,14 @@ import { bookStatus } from '../redux/categories/categories';
 const Category = () => {
   const selectedCategory = useSelector((state) => state.categories);
   const dispatchCategory = useDispatch();
-
+  console.log(selectedCategory);
   const btnCheckStatus = () => {
     dispatchCategory(bookStatus());
   };
   return (
     <div className="Category">
       <div className="Category-text">
-        <h2>{selectedCategory.Categories[0]}</h2>
+        <h3>{selectedCategory.categories[0]}</h3>
         <button onClick={btnCheckStatus} type="button">Check status</button>
       </div>
     </div>
