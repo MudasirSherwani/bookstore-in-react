24b590237430bffeb550d8d7de13418bf9a68622
Dell Pc <mudasir9757@gmail.com>
2023-01-27 11:55:26 +0500

    Add category reducer in cat component

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './Category.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { bookStatus } from '../redux/categories/categories';
 
-const Category = () => (
+
+const Category = () => {
+  const selectedCategory = useSelector((state) => state.categories);
+  const dispatchCategory = useDispatch();
+
+  const btnCheckStatus = () => {
+    dispatchCategory(bookStatus());
+  };
+
+return (
   <div className="Category">
     <div className="Category-text">
-      <button type="button">Check status</button>
+      <h2>{selectedCategory.Categories[0]}</h2>
+      <button onClick={btnCheckStatus} type="button">Check status</button>
     </div>
   </div>
 );
 
+};
 export default Category;
