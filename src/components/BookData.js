a5ef56f98c1919d26aa6bcbfddb0042fecc754d5
Dell Pc <mudasir9757@gmail.com>
2023-01-28 18:12:06 +0500

    Add Styles To Books List and Body

--- src/components/BookData.js
+++ src/components/BookData.js
@@ -5,7 +5,7 @@ import { removeBook } from '../redux/books/booksApi';
 
 const BookData = (props) => {
   const {
-    keyValue, title, author,
+    keyValue, title, author, category,
   } = props;
   const dispatchBooks = useDispatch();
   const removeEvent = () => {
@@ -14,16 +14,45 @@ const BookData = (props) => {
 
   return (
     <div className="book-item">
-      <li>
-        <h2>{title}</h2>
-        <h4>{author}</h4>
-        <button type="button" onClick={removeEvent}>Remove</button>
-      </li>
+      <div className="content">
+        <div className="book-data">
+          <h4 className="book-category">{category}</h4>
+          <h2 className="book-title">{title}</h2>
+          <h6 className="book-author">{author}</h6>
+          <div className="actions">
+            <button className="button-outline" type="button">Comments</button>
+            <div className="v-divider" />
+            <button className="button-outline" type="button" onClick={removeEvent}>Remove</button>
+            <div className="v-divider" />
+            <button className="button-outline" type="button">Edit</button>
+          </div>
+        </div>
+        <div className="progress-container">
+          <div className="circ-progress-container">
+            <div className="circ-progress" />
+          </div>
+          <div className="progress">
+            <p className="percent">72%</p>
+            <p className="completed">Completed</p>
+          </div>
+          <div className="prog-divider" />
+          <div className="chapter-container">
+            <div>
+              <p className="chapter-label">CURRENT CHAPTER</p>
+              <p className="chapter">Introduction</p>
+            </div>
+            <div>
+              <button className="primary-button" type="button">UPDATE PROGRESS</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 BookData.propTypes = {
+  category: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   keyValue: PropTypes.string.isRequired,
--- src/components/BookPage.js
+++ src/components/BookPage.js
@@ -3,10 +3,12 @@ import AddBook from './AddBooksForm';
 import BooksList from './BooksList';
 
 const Books = () => (
-  <>
+
+  <div className="container">
     <BooksList />
+    <div className="horizontal-divider" />
     <AddBook />
-  </>
+  </div>
 );
 
 export default Books;
